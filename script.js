253e58deed400a29b43cb717421b868b25574be1
Nikolay Kus <byrgatskiy@gmail.com>
2017-11-07 11:16:42 +0300

    Update

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
-  $('.add #button').on('click',function(){
-      var template = $('#template').clone();
-      template.removeAttr('id').find('textarea').html($('.add textarea').val());
-      $(template).appendTo('.block');
+$('.add #button').on('click',function(){ /*делаем клон блока добавления*/
+    var template = $('#template').clone();
+    template.removeAttr('id').find('textarea').html($('.add textarea').val());
+    $(template).appendTo('.block');
 })
 
 
-  $('.block').on('click','.close',function(){
-  $(this).parent().remove();
+$('.block').on('click','.close',function(){   /*удаляем блок*/
+$(this).parent().remove();
 })
 
-  $('.block').on('click','#button',function(){
-   $(this).siblings('textarea').removeAttr('disabled');
-   $('html').keydown(function(e){
-  if (e.keyCode == 9) {
-    $('textarea').attr("disabled","disabled");
-  }
+$('.block').on('click','#button',function(){ /*редактирование и по вводу Enter - заканчивается редактирование*/
+ $(this).siblings('textarea').removeAttr('disabled');
+ $('html').keydown(function(e){
+if (e.keyCode == 13) {
+  $('textarea').attr("disabled","disabled");
+}
 });
-  })
+})
+
+$('.sidebar').on('click', 'button', function(){ /*блок рекламы*/
+  $(this).closest('.sidebar').find('.tic').slideDown();
+})
